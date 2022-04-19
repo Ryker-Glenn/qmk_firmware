41094f205c92707c671269ee688128aae5ea7fd3
Ryker Frohock <frohockr@yahoo.com>
2022-04-18 22:26:23 -0400

    more bs

--- keyboards/t2/t2.c
+++ keyboards/t2/t2.c
@@ -3,17 +3,12 @@
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
+        if (clockwise) tap_code(KC_PGDN);
+        else           tap_code(KC_PGUP);
+    }
+    else if (index == 1) {
+        if (clockwise) tap_code(KC_VOLU);
+        else           tap_code(KC_VOLD);
     }
     return false;
 }
